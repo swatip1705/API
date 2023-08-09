4f9fd2ff5f69ada6555b141b1b245a2a23c09746
swatip1705 <71873567+swatip1705@users.noreply.github.com>
2023-08-10 00:09:18 +0530

    Add files via upload

---
+++ main-tf-serving.py
@@ -0,0 +1,67 @@
+from io import BytesIO
+
+import numpy as np
+import tensorflow as tf
+import uvicorn
+from PIL import Image
+from fastapi import FastAPI, UploadFile
+import requests
+from starlette.middleware.cors import CORSMiddleware
+
+app: FastAPI = FastAPI()
+
+origins=[
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+endpoint="http://localhost:8502/v1/models/potatoes_model:predict"
+
+
+class_names=["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive "
+
+def read_file_as_image(data) -> np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+        file: UploadFile
+
+):
+    image= read_file_as_image(await file.read())
+    img_batch=np.expand_dims(image,0)
+
+    json_data={
+        "instances":img_batch.tolist()
+    }
+
+    response= requests.post(endpoint, json=json_data)
+    prediction=np.array(response.json()["predictions"][0])
+    predicted_class=class_names[np.argmax(prediction)]
+    confidence=np.max(prediction)
+
+    return{
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
